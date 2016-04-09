6b820a2c48897d8f189923ea860bae5bf74acecc
Hank Tom <hanktom223@gmail.com>
2016-04-09 23:54:41 +0800

    清單式功能表

--- app/src/main/java/com/tom/atm/MainActivity.java
+++ app/src/main/java/com/tom/atm/MainActivity.java
@@ -6,14 +6,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     boolean logon = false;
     public static final int FUNC_LOGIN = 1;
+    String[] func = {"餘額查詢", "交易明細", "最新消息", "投資理財", "離開"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView list = (ListView) findViewById(R.id.list);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, func);
+        list.setAdapter(adapter);
         if (!logon){ //如果未登入, 則開啟LoginActivity
             Intent intent = new Intent(this, LoginActivity.class);
 //            startActivity(intent);
