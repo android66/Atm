9eef49e7478546dd5205b5e7e998c4e51bbe3f43
Hank Tom <hanktom223@gmail.com>
2016-04-10 15:08:25 +0800

    SQLite-二欄ListView

--- app/src/main/java/com/tom/atm/FinanceActivity.java
+++ app/src/main/java/com/tom/atm/FinanceActivity.java
@@ -1,12 +1,15 @@
 package com.tom.atm;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class FinanceActivity extends AppCompatActivity {
 
@@ -14,6 +17,17 @@ public class FinanceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finance);
+        ListView list = (ListView) findViewById(R.id.list);
+        MyDBHelper helper = new MyDBHelper(this, "expense.db", null, 1);
+        Cursor c = helper.getReadableDatabase().query(
+                "exp", null, null, null, null, null, null);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_expandable_list_item_2,
+                c,
+                new String[] {"info", "amount"},
+                new int[] {android.R.id.text1, android.R.id.text2},
+                0);
+        list.setAdapter(adapter);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
