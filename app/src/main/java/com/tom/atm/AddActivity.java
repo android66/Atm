599b62938b0c6254d04ccad98978e38c83d7ebb0
Hank Tom <hanktom@gmail.com>
2016-07-03 16:29:28 +0800

    AutoCompleteTextView-字串陣列

--- app/src/main/java/com/tom/atm/AddActivity.java
+++ app/src/main/java/com/tom/atm/AddActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -15,13 +17,30 @@ public class AddActivity extends AppCompatActivity {
     private EditText edAmount;
     private MyDBHelper helper;
     private DatePicker datePicker;
+    private AutoCompleteTextView autoInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         findViews();
+        setupAdapter();
         helper = MyDBHelper.getInstance(this);
+    }
+
+    private void setupAdapter() {
+        String[] infos = {"Breakfast", "Bread", "Lunch", "Parking", "Bill"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                infos);
+        autoInfo.setAdapter(adapter);
+        autoInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                autoInfo.showDropDown();
+            }
+        });
     }
 
     private void findViews() {
@@ -29,13 +48,14 @@ public class AddActivity extends AppCompatActivity {
         edInfo = (EditText) findViewById(R.id.ed_info);
         edAmount = (EditText) findViewById(R.id.ed_amount);
         datePicker = (DatePicker) findViewById(R.id.datePicker);
+        autoInfo = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
     }
 
-    public void add(View v){
+    public void add(View v) {
 //        String cdate = edDate.getText().toString();
         // 取得DatePicker並轉換字串
         int year = datePicker.getYear();
-        int month = datePicker.getMonth()+1;
+        int month = datePicker.getMonth() + 1;
         int day = datePicker.getDayOfMonth();
         String cdate = year + "-" + month + "-" + day;
 
@@ -46,6 +66,6 @@ public class AddActivity extends AppCompatActivity {
         values.put("info", info);
         values.put("amount", amount);
         long id = helper.getWritableDatabase().insert("exp", null, values);
-        Log.d("ADD", id+"");
+        Log.d("ADD", id + "");
     }
 }
--- app/src/main/java/com/tom/atm/MyDBHelper.java
+++ app/src/main/java/com/tom/atm/MyDBHelper.java
@@ -1,5 +1,6 @@
 package com.tom.atm;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
